e7258a074df6ccdda0ee59eeb40f2283981211c4
Deividas Balinskis <deividasbalinskis@gmail.com>
2020-11-04 23:28:32 +0200

    Brackeys 13
    spawn particles, sound(doesnt work)

--- Cosmos-Game/Assets/GameMaster.cs
+++ Cosmos-Game/Assets/GameMaster.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,15 +17,19 @@ public class GameMaster : MonoBehaviour
 
     public Transform playerPrefab;
     public Transform spawnPoint;
-    public int spawnDelay = 2;
+    public float spawnDelay = 2;
+    public Transform spawnPrefab;
+    public AudioClip respawnAudio;
 
     public IEnumerator RespawnPlayer()
     {
-        //add waiting sound
+        AudioSource.PlayClipAtPoint(respawnAudio, new Vector3(spawnPoint.position.x, spawnPoint.position.y, spawnPoint.position.z), 0.5f);
+        //GetComponent<AudioSource>().Play();
         yield return new WaitForSeconds(spawnDelay);
 
         Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
-        //add span particles here
+        Transform clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) ;
+        Destroy(clone.gameObject, 3f);
     }
 
     public static void KillPlayer (Player player)
@@ -33,3 +38,31 @@ public class GameMaster : MonoBehaviour
         gm.StartCoroutine(gm.RespawnPlayer());
     }
 }
+/*public class GameMaster : MonoBehaviour
+{
+    public static GameMaster gm;                // GameMaster.cs
+
+    public int spawnDelay = 3;                  // the player respawn delay
+    public Transform playerPrefab;              // the player
+    public Transform spawnPoint;                // the spawn point
+    public GameObject spawnParticlePrefab;
+
+    public AudioSource respawnAudio;
+
+    void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+
+        respawnAudio = GetComponent<AudioSource>();
+    }
+
+    // Wait 2 seconds and respawn the player at the spawn point position
+    public IEnumerator RespawnPlayer()
+    {
+        respawnAudio.Play();
+        yield return new WaitForSeconds(spawnDelay);
+    }
+}*/
