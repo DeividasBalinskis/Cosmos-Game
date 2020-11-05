c5895cfe7ff3670647cbc53bc04136fa821ce0cd
Deividas Balinskis <deividasbalinskis@gmail.com>
2020-11-05 23:37:02 +0200

    Brackeys 17
    Shooting particles added. Shooting bullet changed

--- Cosmos-Game/Assets/Scripts/Weapon.cs
+++ Cosmos-Game/Assets/Scripts/Weapon.cs
@@ -10,6 +10,7 @@ public class Weapon : MonoBehaviour
 
     public Transform BulletTrailPrefab;
     public Transform MuzzleFlashPrefab;
+    public Transform hitPrefab;
     float timeToSpawnEffect = 0;
     public float effectSpawnRate = 10;
 
@@ -50,11 +51,7 @@ public class Weapon : MonoBehaviour
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition, 100, WhatToHit);
-        if(Time.time >= timeToSpawnEffect)
-        {
-            Effect();
-            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
-        }
+
         Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
         if(hit.collider != null)
         {
@@ -66,11 +63,48 @@ public class Weapon : MonoBehaviour
                 Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage");
             }
         }
+
+        if (Time.time >= timeToSpawnEffect)
+        {
+            Vector3 hitPos;
+            Vector3 hitNormal;
+
+            if (hit.collider == null)
+            {
+                hitPos = (mousePosition - firePointPosition) * 9999;
+                hitNormal = new Vector3(9999, 9999, 9999);
+            }
+            else
+            {
+                hitPos = hit.point;
+                hitNormal = hit.normal;
+            }
+
+
+            Effect(hitPos, hitNormal);
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
     }
 
-    void Effect()
+    void Effect(Vector3 hitPos, Vector3 hitNormal)
     {
-        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
+        Transform trail = Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation) as Transform;
+        LineRenderer lr = trail.GetComponent<LineRenderer>();
+
+        if(lr != null)
+        {
+            lr.SetPosition(0, firePoint.position);
+            lr.SetPosition(1, hitPos);
+        }
+
+        Destroy(trail.gameObject, 0.04f);
+
+        if (hitNormal != new Vector3(9999, 9999, 9999)) 
+        {
+            Transform hitParticle = (Transform)Instantiate(hitPrefab, hitPos, Quaternion.FromToRotation(Vector3.right, hitNormal));
+            Destroy(hitParticle.gameObject, 1f);
+        }
+
         Transform clone = (Transform)Instantiate(MuzzleFlashPrefab, firePoint.position, firePoint.rotation);
         clone.parent = firePoint;
         float size = Random.Range(0.3f, 0.5f);
