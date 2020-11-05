90086e7cd7ec40cc9da4ccd6178f447d0055662f
Deividas Balinskis <deividasbalinskis@gmail.com>
2020-11-05 14:11:57 +0200

    Brackeys Enemies 14
    Added A*, Enemy, follow

---
+++ Cosmos-Game/Assets/EnemyAI.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using Pathfinding;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+
+    public float updateRate = 2;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    //The calculated path
+    public Path path;
+
+    //Ai speed
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    [HideInInspector]
+    public bool pathIsEnded = false;
+
+    public float nextWaypointDistance = 3;
+
+    private int currentWaypoint = 0;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        if(target == null)
+        {
+            //target search
+            return;
+        }
+
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        StartCoroutine(UpdatePath());
+    }
+
+    IEnumerator UpdatePath()
+    {
+        if(target == null)
+        {
+            //insert player search
+            yield return false;
+        }
+
+        if(target != null)
+        {
+            seeker.StartPath(transform.position, target.position, OnPathComplete);
+        }
+        
+
+        yield return new WaitForSeconds(1 / updateRate);
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        StartCoroutine(UpdatePath());
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("We got a path. Did it have an error? " + p.error);
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (target == null)
+        {
+            //insert player search
+            return;
+        }
+
+        //always look at a player
+
+        if(path == null)
+        {
+            return;
+        }
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            if (pathIsEnded)
+                return;
+
+            Debug.Log("End of path reached");
+            pathIsEnded = true;
+            return;
+        }
+        pathIsEnded = false;
+
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed * Time.fixedDeltaTime;
+
+        //move AI
+        rb.AddForce(dir, fMode);
+
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+        if (dist < nextWaypointDistance)
+        {
+            currentWaypoint++;
+            return;
+        }
+    }
+}
---
+++ Cosmos-Game/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster gm;
+
+    void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+        /*if(audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }*/  // -*
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public float spawnDelay = 2;
+    public Transform spawnPrefab;
+    //public static AudioSource audioSource; -*
+
+    public IEnumerator RespawnPlayer()
+    {
+        GetComponent<AudioSource>().Play();
+        //audioSource.Play(); -*
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Transform clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) ;
+        Destroy(clone.gameObject, 3f);
+    }
+
+    public static void KillPlayer (Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+}
+/*public class GameMaster : MonoBehaviour
+{
+    public static GameMaster gm;                // GameMaster.cs
+
+    public int spawnDelay = 3;                  // the player respawn delay
+    public Transform playerPrefab;              // the player
+    public Transform spawnPoint;                // the spawn point
+    public GameObject spawnParticlePrefab;
+
+    public AudioSource respawnAudio;
+
+    void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+
+        respawnAudio = GetComponent<AudioSource>();
+    }
+
+    // Wait 2 seconds and respawn the player at the spawn point position
+    public IEnumerator RespawnPlayer()
+    {
+        respawnAudio.Play();
+        yield return new WaitForSeconds(spawnDelay);
+    }
+}*/
